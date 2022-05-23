bc1582c6b7f76773a736ba6c2bc01939ef35983d
Juan Savedra <juansavedra827@gmail.com>
2022-05-23 15:36:46 -0300

    variáveis

---
+++ variáveis.lua
@@ -0,0 +1,31 @@
+--[[
+Number - Numérico 
+valor = 3 / valor = 4.2
+=======================
+String - Texto
+print("Hello world")
+====================
+Boolean - Verdadeiro ou Falso
+valor = true / valor = false
+=============================
+Nil - Nenhum valor atribuído
+============================
+Table - Estrutura de dados composta com índice e valores (como se fosse um objeto)
+personagem = {vida = 100, admin = false, nome = "Savedra"}
+==================================================================================
+Function - Bloco de código reutilizável que retorna ou não um valor
+Função()
+===================================================================
+Userdata - Aponta para um objeto 
+BrickColor
+================================
+]]
+
+local number = 3;
+local name = "Savedra";
+local boolean = false;
+local nilValue;
+local table = { name = "Savedra", age = 15 }
+local function Execute()
+  print("Hello world");
+end
