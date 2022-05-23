a7e4134516a239faff394679752f3d6ac0a282f4
Juan Savedra <juansavedra827@gmail.com>
2022-05-23 15:56:33 -0300

    estruturas de decisão

---
+++ estruturas_de_decisão.lua
@@ -0,0 +1,16 @@
+--[[
+if - Se
+elseif - Se não se
+else - Se não
+]]
+
+local x = 10;
+local y = 10;
+
+if (x == y) then
+  print("X = Y");
+elseif (x ~= 10) then
+  print("X diferente de Y");
+else
+  print("Erro");
+end
