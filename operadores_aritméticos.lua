bc5eca35209043bbd206611c13d60b1ce72cde30
Juan Savedra <juansavedra827@gmail.com>
2022-05-23 15:39:57 -0300

    operadores aritméticos

---
+++ operadores_aritméticos.lua
@@ -0,0 +1,14 @@
+--[[
++ = Adição
+- = Subtração
+* = Multiplicação
+/ = Divisão
+^ = Potência
+]]
+
+local number = 10;
+number = number + 10;
+number = number - 10;
+number = number * 10;
+number = number / 10;
+number = number ^ 2;
